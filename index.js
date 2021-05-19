bd36ff49ef7913206b9bed76c805e8584d9b38b2
Celeste Alpizar <maria.alpizar.72@gmail.com>
2021-05-18 22:25:20 -0600

    Shopping cart ready

--- index.js
+++ index.js
@@ -10,11 +10,20 @@ let cart = {}
 // con este addEventListener obtengo los datos de mi "base de datos" y el DOMContentLoaded se dispara cuando ya el documento se encuentra totalmente cargado
 document.addEventListener("DOMContentLoaded", () => {
     fetchData()
+    if(localStorage.getItem("cart")){
+        cart = JSON.parse(localStorage.getItem("cart"))
+        showCart()
+    }
 })
 
 cards.addEventListener("click", e => {
     addToCart(e)
 })
+
+items.addEventListener("click", e => {
+    btnAction(e)
+})
+
 
 // Traigo los productos
 const fetchData = async () => {
@@ -22,14 +31,14 @@ const fetchData = async () => {
         const res = await fetch("api.json")
         const data = await res.json()
         // console.log(data)
-        mostrarCard(data)
+        showCard(data)
     } catch (error) {
         console.log(error)
     }
 }
 
 // Muestro los productos
-const mostrarCard = data => {
+const showCard = data => {
     // uso el forEach porque el api es un json 
     data.forEach(product => {
         templateCard.querySelector("h5").textContent = product.title
@@ -67,9 +76,75 @@ const setCart = objeto =>{
     }
 
     cart[product.id] = {...product}
-    mostrarCart()
+    showCart()
 }
 
-const mostrarCart = () => {
-    console.log(cart)
+const showCart = () => {
+    // console.log(cart)
+    items.innerHTML = ""
+    Object.values(cart).forEach(product =>{
+        templateCart.querySelector("th").textContent = product.id 
+        templateCart.querySelectorAll("td")[0].textContent = product.title
+        templateCart.querySelectorAll("td")[1].textContent = product.quantity
+        templateCart.querySelector(".btn-info").dataset.id = product.id
+        templateCart.querySelector(".btn-danger").dataset.id = product.id
+        templateCart.querySelector("span").textContent = product.quantity * product.price
+
+        const clone = templateCart.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
+
+    showFooter()
+
+    localStorage.setItem("cart", JSON.stringify(cart))
+}
+
+const showFooter = () => {
+    footer.innerHTML = ""
+    if (Object.keys(cart).length === 0){
+        footer.innerHTML = `<th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>`
+
+        return
+    }
+
+    const nQuantity = Object.values(cart).reduce((acc, {quantity}) => acc + quantity, 0)
+    const nPrice = Object.values(cart).reduce((acc, {quantity, price}) => acc + quantity * price, 0)
+    
+    templateFooter.querySelectorAll("td")[0].textContent = nQuantity
+    templateFooter.querySelector("span").textContent = nPrice
+
+    const clone = templateFooter.cloneNode(true)
+    fragment.appendChild(clone)
+    footer.appendChild(fragment)
+
+    const btnClearAll = document.getElementById("clear-cart")
+    btnClearAll.addEventListener("click", () => {
+        cart = {}
+        showCart()
+    })
+}
+
+const btnAction = e => {
+    // console.log(e.target)
+    // para aumentar la cantidad
+    if(e.target.classList.contains("btn-info")){
+        console.log(cart[e.target.dataset.id])
+        // cart[e.target.dataset.id]
+        const product = cart[e.target.dataset.id]
+        product.quantity = cart[e.target.dataset.id].quantity + 1        // se puede reemplazar todo lo que esta desde el = por ++
+        cart[e.target.dataset.id] = {...product}
+        showCart()
+    }
+
+    if(e.target.classList.contains("btn-danger")){
+        const product = cart[e.target.dataset.id]
+        product.quantity = cart[e.target.dataset.id].quantity - 1        // se puede reemplazar todo lo que esta desde el = por --
+        if(product.quantity === 0){
+            delete cart[e.target.dataset.id]
+        }
+        showCart()
+    }
+
+    e.stopPropagation()
 }
